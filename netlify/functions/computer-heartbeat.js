d241fea53ee6d9bbb38963d7fbf596eacf2327ed
hambalifachri <igneelmyth@gmail.com>
2026-08-21 13:46:47 +0700

    Extend NIM login sessions to four hours

--- netlify/functions/computer-heartbeat.js
+++ netlify/functions/computer-heartbeat.js
@@ -6,7 +6,7 @@ const supabase = createClient(
 );
 
 const SESSION_TIMEOUT_MS = 30 * 60 * 1000;
-const SESSION_MAX_MS = 2 * 60 * 60 * 1000;
+const SESSION_MAX_MS = 4 * 60 * 60 * 1000;
 const FREE_ACCESS_MAX_MS = 12 * 60 * 60 * 1000;
 
 function response(statusCode, data) {
